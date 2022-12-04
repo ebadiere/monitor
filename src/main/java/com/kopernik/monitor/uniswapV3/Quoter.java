32aa78535ab3fb680b7efdfc3289e51473793795
ericbadiere <eric.badiere@coinme.com>
2022-12-03 22:18:08 -0700

    WIP. Still a mess, but working with price.

--- src/main/java/com/kopernik/monitor/uniswapV3/Quoter.java
+++ src/main/java/com/kopernik/monitor/uniswapV3/Quoter.java
@@ -1,6 +1,8 @@
 package com.kopernik.monitor.uniswapV3;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import lombok.extern.slf4j.Slf4j;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.tuples.generated.Tuple7;
@@ -9,6 +11,7 @@ import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.DefaultGasProvider;
 import com.zuehlke.blockchain.model.IUniswapV3Pool;
 
+@Slf4j
 public class Quoter {
 
   String SHIB = "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE";
@@ -24,6 +27,15 @@ public class Quoter {
   public String getQuoteForSinglePool() throws Exception {
 
     Tuple7<BigInteger, BigInteger, BigInteger, BigInteger, BigInteger, BigInteger, Boolean> slot0 = iUniswapV3Pool.slot0().send();
+    BigInteger sqrtRatioX96 = slot0.component1();
+    BigInteger price = sqrtRatioX96.pow(2).divide(BigInteger.valueOf(2).pow(192));
+
+    BigDecimal numerator = new BigDecimal(sqrtRatioX96.pow(2));
+    BigDecimal denominator = new BigDecimal((BigInteger.valueOf(2).pow(192)));
+
+    BigDecimal verifiedPrice = numerator.divide(denominator);
+
+    log.info("Price: {}", verifiedPrice.toPlainString());
 
 
     return "tooMuch";
